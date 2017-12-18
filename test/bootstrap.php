86903db1cdd048a13f97c0ab5620e3e6f37125bc
George Steel <george@net-glue.co.uk>
2017-12-18 23:58:47 +0000

    Bin redundant bootstrapping stuff and fixtures

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,32 +1,3 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
-
-class Bootstrap
-{
-
-    public $container;
-    public $app;
-
-    public static $instance;
-
-    private function __construct()
-    {
-        $this->container = require __DIR__ . '/config/container.php';
-        $app = $this->app = $container->get(\Zend\Expressive\Application::class);
-        self::$instance = $this;
-        require_once __DIR__ . '/config/pipeline.php';
-    }
-
-    public static function getInstance()
-    {
-        if (!self::$instance) {
-            new self;
-        }
-        return self::$instance;
-    }
-}
-
-Bootstrap::getInstance();
-
