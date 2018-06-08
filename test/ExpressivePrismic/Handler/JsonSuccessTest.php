a89b078d0b0791a13bdf3e403fe329a19ce49b43
George Steel <george@net-glue.co.uk>
2018-06-08 09:15:48 +0100

    Additional coverage for JsonSuccess request handler

--- test/ExpressivePrismic/Handler/JsonSuccessTest.php
+++ test/ExpressivePrismic/Handler/JsonSuccessTest.php
@@ -11,6 +11,7 @@ use ExpressivePrismic\Handler\JsonSuccess;
 
 // Deps
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface;
 use Zend\Diactoros\Response\JsonResponse;
 
 class JsonSuccessTest extends TestCase
@@ -23,6 +24,20 @@ class JsonSuccessTest extends TestCase
         $middleware = new JsonSuccess;
 
         $response = $middleware->handle($request);
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertSame(200, $response->getStatusCode());
+    }
+
+    public function testJsonIsReturnedFromProcess()
+    {
+        $request = $this->prophesize(Request::class)->reveal();
+
+        $middleware = new JsonSuccess;
+        /** @var RequestHandlerInterface $delegate */
+        $delegate = $this->prophesize(RequestHandlerInterface::class);
+        $delegate->handle()->shouldNotBeCalled();
+        $response = $middleware->process($request, $delegate->reveal());
 
         $this->assertInstanceOf(JsonResponse::class, $response);
         $this->assertSame(200, $response->getStatusCode());
