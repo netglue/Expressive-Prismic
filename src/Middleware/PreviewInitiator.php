df49b16492fe6c8bc0cc54e3b543fdc9bbfb0152
George Steel <george@net-glue.co.uk>
2018-11-15 18:02:04 +0000

    Changed preview initiator so that it no longer sets a cookie. The JS toolbar now sets this for you and if you want previews, you need to have it installed anyway. This double setting of the preview cookie has been causing problems when both are successfully set. The middleware also checks for an ExpiredPreviewTokenException and presents a more friendly error message for previewers. This exception only became available in the PHP Kit version 4.2, just released, so composer.json is updated to require it

--- src/Middleware/PreviewInitiator.php
+++ src/Middleware/PreviewInitiator.php
@@ -3,12 +3,14 @@ declare(strict_types=1);
 
 namespace ExpressivePrismic\Middleware;
 
+use ExpressivePrismic\Exception\RuntimeException;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface as DelegateInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Prismic;
-use Zend\Http\Header\SetCookie;
+use Prismic\Exception as PrismicException;
+use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\RedirectResponse;
 
 class PreviewInitiator implements MiddlewareInterface
@@ -35,33 +37,39 @@ class PreviewInitiator implements MiddlewareInterface
         $token = urldecode($query['token']);
 
         /**
-         * If you don't set the cookie, the Prismic Preview Icon will not show up
-         * at the bottom of the page
-         */
-
-        /**
-         * @todo Ideally cookie expiry would be configurable
-         */
-        $expires = time() + (29 * 60);
-
-        /** @var \Psr\Http\Message\UriInterface */
-        $uri = $request->getUri();
-
-        $cookie = new SetCookie(
-            Prismic\Api::PREVIEW_COOKIE,
-            $token,
-            $expires,
-            null, // $path - Can't see a use case for limiting to specific path right now
-            $uri->getHost(), // $domain
-            ($uri->getScheme() === 'https'), // $secure - true if current scheme is
-            false // $httpOnly - Nope, JS needs this cookie
-        );
-
-        /**
          * Figure out URL and redirect
          */
-        $url = $this->api->previewSession($token, '/');
+        try {
+            $url = $this->api->previewSession($token, '/');
+            return new RedirectResponse($url, 302);
+        } catch (PrismicException\ExceptionInterface $exception) {
+            /**
+             * If possible return a more friendly error message for the relatively common occurrence that a
+             * preview token has expired
+             */
+            if ($exception instanceof PrismicException\ExpiredPreviewTokenException) {
+                return $this->generatePreviewExpiredError();
+            }
+            throw new RuntimeException(
+                'An unknown error has occurred',
+                500,
+                $exception
+            );
+        }
+    }
 
-        return new RedirectResponse($url, 302, [$cookie->getFieldName() => $cookie->getFieldValue()]);
+    private function generatePreviewExpiredError() : Response
+    {
+        $responseBody = <<<EOF
+<html>
+<head><title>Error: Preview Expired</title></head>
+<body>
+<h1>Preview Token Expired</h1>
+<p>This error occurs when you re-use an out of date link to preview content.</p>
+<p>Simply <a href="/">navigate away</a> from this error, or start a fresh preview session from within the CMS.</p>
+</body>
+</html>
+EOF;
+        return new HtmlResponse($responseBody, 410);
     }
 }
