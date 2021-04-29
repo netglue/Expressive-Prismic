c0f0e4eb158cbbca14a4c066d63e3d10c45bc9e5
George Steel <george@net-glue.co.uk>
2021-04-29 08:39:10 +0100

    Update deps to allow for recent versions of laminas/diactoros
    Requires laminas-diactoros instead of zend* and requires the zendframework bridge
    Removes PHPStan as it is mostly unused and causes conflicts
    Updates PHPUnit to recent version along with the Prophecy Trait so tests work.
    Bumps minimum PHP to 7.3

--- test/ExpressivePrismic/TestCase.php
+++ test/ExpressivePrismic/TestCase.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 namespace ExpressivePrismicTest;
 
 use PHPUnit\Framework\TestCase as PHPUnit;
+use Prophecy\PhpUnit\ProphecyTrait;
 
 class TestCase extends PHPUnit
 {
+    use ProphecyTrait;
+
     protected function tearDown() : void
     {
         $refl = new \ReflectionObject($this);
